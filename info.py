09c96a9e93dc5680d127a7d042e15ee43ee226a0
New experiment <164980847+Subhajit722160@users.noreply.github.com>
2024-12-23 20:18:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SGBACKUP')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SGBACKUP')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SGBACKUP') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/SubhajitGhosh0')
 
 # True Or False
